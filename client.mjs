135053c7c4da3bf069ea2cfc6f71db684e2ba5c7
Jersey Wecksell <49763206+JerseyWecksell@users.noreply.github.com>
2020-06-23 00:48:47 +0400

    Add files via upload

---
+++ client.mjs
@@ -0,0 +1,632 @@
+//By Jersey
+//Version 1.0.0
+
+import * as alt from 'alt';
+
+//Market24/7  blips - 18
+
+alt.onServer('MarketA', (player)=> {
+	let marketA = new alt.PointBlip(28.747108459472656, -1347.2559814453125, 0);
+	marketA.sprite = 52;
+	marketA.name = 'Магазин24/7';
+	marketA.scale = 0.9;
+	marketA.shortRange = true;
+});
+
+alt.onServer('MarketB', (player)=> {
+	let marketB = new alt.PointBlip(-3240.854248046875, 1004.1343994140625, 0);
+	marketB.sprite = 52;
+	marketB.name = 'Магазин24/7';
+	marketB.scale = 0.9;
+	marketB.shortRange = true;
+});
+
+alt.onServer('MarketC', (player)=> {
+	let marketC = new alt.PointBlip(-55.15660858154297, -1756.1046142578125, 0);
+	marketC.sprite = 52;
+	marketC.name = 'Магазин24/7';
+	marketC.scale = 0.9;
+	marketC.shortRange = true;
+});
+
+alt.onServer('MarketD', (player)=> {
+	let marketD = new alt.PointBlip(1137.80419921875, -980.0777587890625, 0);
+	marketD.sprite = 52;
+	marketD.name = 'Магазин24/7';
+	marketD.scale = 0.9;
+	marketD.shortRange = true;
+});
+
+alt.onServer('MarketF', (player)=> {
+	let marketF = new alt.PointBlip(1159.2828369140625, -322.74957275390625, 0);
+	marketF.sprite = 52;
+	marketF.name = 'Магазин24/7';
+	marketF.scale = 0.9;
+	marketF.shortRange = true;
+});
+
+alt.onServer('MarketG', (player)=> {
+	let marketG = new alt.PointBlip(376.7419738769531, 322.868408203125, 0);
+	marketG.sprite = 52;
+	marketG.name = 'Магазин24/7';
+	marketG.scale = 0.9;
+	marketG.shortRange = true;
+});
+
+alt.onServer('MarketI', (player)=> {
+	let marketI = new alt.PointBlip(-711.8973999023438, -920.9361572265625, 0);
+	marketI.sprite = 52;
+	marketI.name = 'Магазин24/7';
+	marketI.scale = 0.9;
+	marketI.shortRange = true;
+});
+
+alt.onServer('MarketJ', (player)=> {
+	let marketJ = new alt.PointBlip(-1234.45458984375, -900.7244262695312, 0);
+	marketJ.sprite = 52;
+	marketJ.name = 'Магазин24/7';
+	marketJ.scale = 0.9;
+	marketJ.shortRange = true;
+});
+
+alt.onServer('MarketK', (player)=> {
+	let marketK = new alt.PointBlip(-1495.65869140625, -388.8952941894531, 0);
+	marketK.sprite = 52;
+	marketK.name = 'Магазин24/7';
+	marketK.scale = 0.9;
+	marketK.shortRange = true;
+});
+
+alt.onServer('MarketL', (player)=> {
+	let marketL = new alt.PointBlip(-3034.086669921875, 590.6148681640625, 0);
+	marketL.sprite = 52;
+	marketL.name = 'Магазин24/7';
+	marketL.scale = 0.9;
+	marketL.shortRange = true;
+});
+
+alt.onServer('MarketM', (player)=> {
+	let marketM = new alt.PointBlip(-2978.488525390625, 389.85003662109375, 0);
+	marketM.sprite = 52;
+	marketM.name = 'Магазин24/7';
+	marketM.scale = 0.9;
+	marketM.shortRange = true;
+});
+
+alt.onServer('MarketN', (player)=> {
+	let marketN = new alt.PointBlip(1731.2958984375, 6405.9521484375, 0);
+	marketN.sprite = 52;
+	marketN.name = 'Магазин24/7';
+	marketN.scale = 0.9;
+	marketN.shortRange = true;
+});
+
+alt.onServer('MarketO', (player)=> {
+	let marketO = new alt.PointBlip(1696.5426025390625, 4929.91796875, 0);
+	marketO.sprite = 52;
+	marketO.name = 'Магазин24/7';
+	marketO.scale = 0.9;
+	marketO.shortRange = true;
+});
+
+alt.onServer('MarketP', (player)=> {
+	let marketP = new alt.PointBlip(1970.764892578125, 3737.739990234375, 0);
+	marketP.sprite = 52;
+	marketP.name = 'Магазин24/7';
+	marketP.scale = 0.9;
+	marketP.shortRange = true;
+});
+
+alt.onServer('MarketR', (player)=> {
+	let marketR = new alt.PointBlip(1395.28125, 3597.538818359375, 0);
+	marketR.sprite = 52;
+	marketR.name = 'Магазин24/7';
+	marketR.scale = 0.9;
+	marketR.shortRange = true;
+});
+
+alt.onServer('MarketS', (player)=> {
+	let marketS = new alt.PointBlip(2685.3642578125, 3281.749267578125, 0);
+	marketS.sprite = 52;
+	marketS.name = 'Магазин24/7';
+	marketS.scale = 0.9;
+	marketS.shortRange = true;
+});
+
+alt.onServer('MarketT', (player)=> {
+	let marketT = new alt.PointBlip(1166.074462890625, 2701.6728515625, 0);
+	marketT.sprite = 52;
+	marketT.name = 'Магазин24/7';
+	marketT.scale = 0.9;
+	marketT.shortRange = true;
+});
+
+alt.onServer('MarketU', (player)=> {
+	let marketU = new alt.PointBlip(544.7569580078125, 2676.47412109375, 0);
+	marketU.sprite = 52;
+	marketU.name = 'Магазин24/7';
+	marketU.scale = 0.9;
+	marketU.shortRange = true;
+});
+
+//---//
+
+//Refills - 18
+
+alt.onServer('RefillA', (player)=> {
+	let refillA = new alt.PointBlip(-2552.3681640625, 2321.4912109375, 0);
+	refillA.sprite = 361;
+	refillA.name = 'Заправка';
+	refillA.scale = 0.7;
+	refillA.shortRange = true;
+	refillA.color = 1;
+});
+
+alt.onServer('RefillB', (player)=> {
+	let refillB = new alt.PointBlip(174.92364501953125, 6602.306640625, 0);
+	refillB.sprite = 361;
+	refillB.name = 'Заправка';
+	refillB.scale = 0.7;
+	refillB.shortRange = true;
+	refillB.color = 1;
+});
+
+alt.onServer('RefillC', (player)=> {
+	let refillC = new alt.PointBlip(-2094.715087890625, -319.81524658203125, 0);
+	refillC.sprite = 361;
+	refillC.name = 'Заправка';
+	refillC.scale = 0.7;
+	refillC.shortRange = true;
+	refillC.color = 1;
+});
+
+alt.onServer('RefillD', (player)=> {
+	let refillD = new alt.PointBlip(1699.888427734375, 6420.19091796875, 0);
+	refillD.sprite = 361;
+	refillD.name = 'Заправка';
+	refillD.scale = 0.7;
+	refillD.shortRange = true;
+	refillD.color = 1;
+});
+
+alt.onServer('RefillE', (player)=> {
+	let refillE = new alt.PointBlip(2006.184326171875, 3777.931640625, 0);
+	refillE.sprite = 361;
+	refillE.name = 'Заправка';
+	refillE.scale = 0.7;
+	refillE.shortRange = true;
+	refillE.color = 1;
+});
+
+alt.onServer('RefillF', (player)=> {
+	let refillF = new alt.PointBlip(1783.0672607421875, 3329.734130859375, 0);
+	refillF.sprite = 361;
+	refillF.name = 'Заправка';
+	refillF.scale = 0.7;
+	refillF.shortRange = true;
+	refillF.color = 1;
+});
+
+alt.onServer('RefillG', (player)=> {
+	let refillG = new alt.PointBlip(2678.1953125, 3267.419189453125, 0);
+	refillG.sprite = 361;
+	refillG.name = 'Заправка';
+	refillG.scale = 0.7;
+	refillG.shortRange = true;
+	refillG.color = 1;
+});
+
+alt.onServer('RefillH', (player)=> {
+	let refillH = new alt.PointBlip(1205.1517333984375, 2658.40185546875, 0);
+	refillH.sprite = 361;
+	refillH.name = 'Заправка';
+	refillH.scale = 0.7;
+	refillH.shortRange = true;
+	refillH.color = 1;
+});
+
+alt.onServer('RefillI', (player)=> {
+	let refillI = new alt.PointBlip(50.280609130859375, 2781.439208984375, 0);
+	refillI.sprite = 361;
+	refillI.name = 'Заправка';
+	refillI.scale = 0.7;
+	refillI.shortRange = true;
+	refillI.color = 1;
+});
+
+alt.onServer('RefillJ', (player)=> {
+	let refillJ = new alt.PointBlip(2569.2255859375, 363.5943908691406, 0);
+	refillJ.sprite = 361;
+	refillJ.name = 'Заправка';
+	refillJ.scale = 0.7;
+	refillJ.shortRange = true;
+	refillJ.color = 1;
+});
+
+alt.onServer('RefillK', (player)=> {
+	let refillK = new alt.PointBlip(1188.0093994140625, -335.4875183105469, 0);
+	refillK.sprite = 361;
+	refillK.name = 'Заправка';
+	refillK.scale = 0.7;
+	refillK.shortRange = true;
+	refillK.color = 1;
+});
+
+alt.onServer('RefillL', (player)=> {
+	let refillL = new alt.PointBlip(624.5497436523438, 267.007568359375, 0);
+	refillL.sprite = 361;
+	refillL.name = 'Заправка';
+	refillL.scale = 0.7;
+	refillL.shortRange = true;
+	refillL.color = 1;
+});
+
+alt.onServer('RefillM', (player)=> {
+	let refillM = new alt.PointBlip(-1427.9024658203125, -274.94879150390625, 0);
+	refillM.sprite = 361;
+	refillM.name = 'Заправка';
+	refillM.scale = 0.7;
+	refillM.shortRange = true;
+	refillM.color = 1;
+});
+
+alt.onServer('RefillN', (player)=> {
+	let refillN = new alt.PointBlip(268.1400146484375, -1268.76416015625, 0);
+	refillN.sprite = 361;
+	refillN.name = 'Заправка';
+	refillN.scale = 0.7;
+	refillN.shortRange = true;
+	refillN.color = 1;
+});
+
+alt.onServer('RefillO', (player)=> {
+	let refillO = new alt.PointBlip(-317.7008361816406, -1465.8690185546875, 0);
+	refillO.sprite = 361;
+	refillO.name = 'Заправка';
+	refillO.scale = 0.7;
+	refillO.shortRange = true;
+	refillO.color = 1;
+});
+
+alt.onServer('RefillP', (player)=> {
+	let refillP = new alt.PointBlip(-72.32489776611328, -1755.3048095703125, 0);
+	refillP.sprite = 361;
+	refillP.name = 'Заправка';
+	refillP.scale = 0.7;
+	refillP.shortRange = true;
+	refillP.color = 1;
+});
+
+alt.onServer('RefillR', (player)=> {
+	let refillR = new alt.PointBlip(814.399169921875, -1027.2962646484375, 0);
+	refillR.sprite = 361;
+	refillR.name = 'Заправка';
+	refillR.scale = 0.7;
+	refillR.shortRange = true;
+	refillR.color = 1;
+});
+
+alt.onServer('RefillS', (player)=> {
+	let refillS = new alt.PointBlip(1210.635986328125, -1400.921875, 0);
+	refillS.sprite = 361;
+	refillS.name = 'Заправка';
+	refillS.scale = 0.7;
+	refillS.shortRange = true;
+	refillS.color = 1;
+});
+
+//---//
+
+//Clothing store - 15
+
+alt.onServer('ClothA', (player)=> {
+	let clothA = new alt.PointBlip(-1.6851465702056885, 6518.17138671875, 0);
+	clothA.sprite = 73;
+	clothA.name = 'Магазин одежды';
+	clothA.scale = 0.9;
+	clothA.shortRange = true;
+});
+
+alt.onServer('ClothB', (player)=> {
+	let clothB = new alt.PointBlip(-3170.193603515625, 1051.4559326171875, 0);
+	clothB.sprite = 73;
+	clothB.name = 'Магазин одежды';
+	clothB.scale = 0.9;
+	clothB.shortRange = true;
+});
+
+alt.onServer('ClothC', (player)=> {
+	let clothC = new alt.PointBlip(1684.427490234375, 4820.60546875, 0);
+	clothC.sprite = 73;
+	clothC.name = 'Магазин одежды';
+	clothC.scale = 0.9;
+	clothC.shortRange = true;
+});
+
+alt.onServer('ClothD', (player)=> {
+	let clothD = new alt.PointBlip(1198.562744140625, 2698.069091796875, 0);
+	clothD.sprite = 73;
+	clothD.name = 'Магазин одежды';
+	clothD.scale = 0.9;
+	clothD.shortRange = true;
+});
+
+alt.onServer('ClothE', (player)=> {
+	let clothE = new alt.PointBlip(618.3920288085938, 2748.959716796875, 0);
+	clothE.sprite = 73;
+	clothE.name = 'Магазин одежды';
+	clothE.scale = 0.9;
+	clothE.shortRange = true;
+});
+
+alt.onServer('ClothF', (player)=> {
+	let clothF = new alt.PointBlip(-1093.0888671875, 2703.804931640625, 0);
+	clothF.sprite = 73;
+	clothF.name = 'Магазин одежды';
+	clothF.scale = 0.9;
+	clothF.shortRange = true;
+});
+
+alt.onServer('ClothG', (player)=> {
+	let clothG = new alt.PointBlip(127.42082214355469, -207.0868682861328, 0);
+	clothG.sprite = 73;
+	clothG.name = 'Магазин одежды';
+	clothG.scale = 0.9;
+	clothG.shortRange = true;
+});
+
+alt.onServer('ClothH', (player)=> {
+	let clothH = new alt.PointBlip(-1458.4273681640625, -230.0404510498047, 0);
+	clothH.sprite = 73;
+	clothH.name = 'Магазин одежды';
+	clothH.scale = 0.9;
+	clothH.shortRange = true;
+});
+
+alt.onServer('ClothI', (player)=> {
+	let clothI = new alt.PointBlip(-1203.2855224609375, -782.8414916992188, 0);
+	clothI.sprite = 73;
+	clothI.name = 'Магазин одежды';
+	clothI.scale = 0.9;
+	clothI.shortRange = true;
+});
+
+alt.onServer('ClothJ', (player)=> {
+	let clothJ = new alt.PointBlip(-718.4264526367188, -157.1665802001953, 0);
+	clothJ.sprite = 73;
+	clothJ.name = 'Магазин одежды';
+	clothJ.scale = 0.9;
+	clothJ.shortRange = true;
+});
+
+alt.onServer('ClothK', (player)=> {
+	let clothK = new alt.PointBlip(-152.44046020507812, -306.23150634765625, 0);
+	clothK.sprite = 73;
+	clothK.name = 'Магазин одежды';
+	clothK.scale = 0.9;
+	clothK.shortRange = true;
+});
+
+alt.onServer('ClothL', (player)=> {
+	let clothL = new alt.PointBlip(415.68194580078125, -809.8568725585938, 0);
+	clothL.sprite = 73;
+	clothL.name = 'Магазин одежды';
+	clothL.scale = 0.9;
+	clothL.shortRange = true;
+});
+
+alt.onServer('ClothM', (player)=> {
+	let clothM = new alt.PointBlip(84.72698974609375, -1389.26416015625, 0);
+	clothM.sprite = 73;
+	clothM.name = 'Магазин одежды';
+	clothM.scale = 0.9;
+	clothM.shortRange = true;
+});
+
+alt.onServer('ClothN', (player)=> {
+	let clothN = new alt.PointBlip(-819.1187744140625, -1077.1988525390625, 0);
+	clothN.sprite = 73;
+	clothN.name = 'Магазин одежды';
+	clothN.scale = 0.9;
+	clothN.shortRange = true;
+});
+
+alt.onServer('ClothO', (player)=> {
+	let clothO = new alt.PointBlip(-1333.4013671875, -1283.6395263671875, 0);
+	clothO.sprite = 73;
+	clothO.name = 'Магазин одежды';
+	clothO.scale = 0.9;
+	clothO.shortRange = true;
+});
+
+//---//
+
+//Barbershop - 6
+
+alt.onServer('BarbA', (player)=> {
+	let barbA = new alt.PointBlip(1934.6756591796875, 3722.147216796875, 0);
+	barbA.sprite = 71;
+	barbA.name = 'Барбершоп';
+	barbA.scale = 0.9;
+	barbA.shortRange = true;
+});
+
+alt.onServer('BarbB', (player)=> {
+	let barbB = new alt.PointBlip(1202.8204345703125, -468.9892272949219, 0);
+	barbB.sprite = 71;
+	barbB.name = 'Барбершоп';
+	barbB.scale = 0.9;
+	barbB.shortRange = true;
+});
+
+alt.onServer('BarbC', (player)=> {
+	let barbC = new alt.PointBlip(-29.50437355041504, -143.00001525878906, 0);
+	barbC.sprite = 71;
+	barbC.name = 'Барбершоп';
+	barbC.scale = 0.9;
+	barbC.shortRange = true;
+});
+
+alt.onServer('BarbD', (player)=> {
+	let barbD = new alt.PointBlip(-826.1328125, -190.19180297851562, 0);
+	barbD.sprite = 71;
+	barbD.name = 'Барбершоп';
+	barbD.scale = 0.9;
+	barbD.shortRange = true;
+});
+
+alt.onServer('BarbE', (player)=> {
+	let barbE = new alt.PointBlip(132.24513244628906, -1716.2811279296875, 0);
+	barbE.sprite = 71;
+	barbE.name = 'Барбершоп';
+	barbE.scale = 0.9;
+	barbE.shortRange = true;
+});
+
+alt.onServer('BarbF', (player)=> {
+	let barbF = new alt.PointBlip(-29.50437355041504, -143.00001525878906, 0);
+	barbF.sprite = 71;
+	barbF.name = 'Барбершоп';
+	barbF.scale = 0.9;
+	barbF.shortRange = true;
+});
+
+//---//
+
+//Tattoo parlor - 6
+
+alt.onServer('TattoA', (player)=> {
+	let tattoA = new alt.PointBlip(-287.687255859375, 6201.62158203125, 0);
+	tattoA.sprite = 75;
+	tattoA.name = 'Тату салон';
+	tattoA.scale = 0.9;
+	tattoA.shortRange = true;
+});
+
+alt.onServer('TattoB', (player)=> {
+	let tattoB = new alt.PointBlip(-3169.624267578125, 1076.375244140625, 0);
+	tattoB.sprite = 75;
+	tattoB.name = 'Тату салон';
+	tattoB.scale = 0.9;
+	tattoB.shortRange = true;
+});
+
+alt.onServer('TattoC', (player)=> {
+	let tattoC = new alt.PointBlip(1857.5653076171875, 3748.73779296875, 0);
+	tattoC.sprite = 75;
+	tattoC.name = 'Тату салон';
+	tattoC.scale = 0.9;
+	tattoC.shortRange = true;
+});
+
+alt.onServer('TattoD', (player)=> {
+	let tattoD = new alt.PointBlip(322.08685302734375, 180.48257446289062, 0);
+	tattoD.sprite = 75;
+	tattoD.name = 'Тату салон';
+	tattoD.scale = 0.9;
+	tattoD.shortRange = true;
+});
+
+alt.onServer('TattoE', (player)=> {
+	let tattoE = new alt.PointBlip(-1157.2362060546875, -1422.4189453125, 0);
+	tattoE.sprite = 75;
+	tattoE.name = 'Тату салон';
+	tattoE.scale = 0.9;
+	tattoE.shortRange = true;
+});
+
+alt.onServer('TattoF', (player)=> {
+	let tattoF = new alt.PointBlip(1320.4656982421875, -1648.513671875, 0);
+	tattoF.sprite = 75;
+	tattoF.name = 'Тату салон';
+	tattoF.scale = 0.9;
+	tattoF.shortRange = true;
+});
+
+//---//
+
+//Gun shop - 10
+
+alt.onServer('GunA', (player)=> {
+	let gunA = new alt.PointBlip(-319.7820129394531, 6074.55322265625, 0);
+	gunA.sprite = 567;
+	gunA.name = 'Gun Shop';
+	gunA.scale = 0.9;
+	gunA.shortRange = true;
+});
+
+alt.onServer('GunB', (player)=> {
+	let gunB = new alt.PointBlip(-3169.861328125, 1084.9344482421875, 0);
+	gunB.sprite = 567;
+	gunB.name = 'Gun Shop';
+	gunB.scale = 0.9;
+	gunB.shortRange = true;
+});
+
+alt.onServer('GunC', (player)=> {
+	let gunC = new alt.PointBlip(-1115.42822265625, 2692.51806640625, 0);
+	gunC.sprite = 567;
+	gunC.name = 'Gun Shop';
+	gunC.scale = 0.9;
+	gunC.shortRange = true;
+});
+
+alt.onServer('GunD', (player)=> {
+	let gunD = new alt.PointBlip(1699.8253173828125, 3749.97412109375, 0);
+	gunD.sprite = 567;
+	gunD.name = 'Gun Shop';
+	gunD.scale = 0.9;
+	gunD.shortRange = true;
+});
+
+alt.onServer('GunE', (player)=> {
+	let gunE = new alt.PointBlip(2568.94677734375, 308.6261291503906, 0);
+	gunE.sprite = 567;
+	gunE.name = 'Gun Shop';
+	gunE.scale = 0.9;
+	gunE.shortRange = true;
+});
+
+alt.onServer('GunF', (player)=> {
+	let gunF = new alt.PointBlip(240.48458862304688, -44.05666732788086, 0);
+	gunF.sprite = 567;
+	gunF.name = 'Gun Shop';
+	gunF.scale = 0.9;
+	gunF.shortRange = true;
+});
+
+alt.onServer('GunG', (player)=> {
+	let gunG = new alt.PointBlip(-1315.5396728515625, -389.5924377441406, 0);
+	gunG.sprite = 567;
+	gunG.name = 'Gun Shop';
+	gunG.scale = 0.9;
+	gunG.shortRange = true;
+});
+
+alt.onServer('GunH', (player)=> {
+	let gunH = new alt.PointBlip(-664.2195434570312, -946.139404296875, 0);
+	gunH.sprite = 567;
+	gunH.name = 'Gun Shop';
+	gunH.scale = 0.9;
+	gunH.shortRange = true;
+});
+
+alt.onServer('GunI', (player)=> {
+	let gunI = new alt.PointBlip(13.2644681930542, -1119.8863525390625, 0);
+	gunI.sprite = 567;
+	gunI.name = 'Gun Shop';
+	gunI.scale = 0.9;
+	gunI.shortRange = true;
+});
+
+alt.onServer('GunJ', (player)=> {
+	let gunJ = new alt.PointBlip(844.3624267578125, -1019.0262451171875, 0);
+	gunJ.sprite = 567;
+	gunJ.name = 'Gun Shop';
+	gunJ.scale = 0.9;
+	gunJ.shortRange = true;
+});
+
+//---//
+
+//Banks
